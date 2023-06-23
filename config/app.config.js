2e1172e1f2b8a7db49fe789a507fc493b18eaaba
Ronsard Carnegie <ronsard44djoumessi@gmail.com>
2023-06-23 18:58:49 +0200

    backend commit

---
+++ config/app.config.js
@@ -0,0 +1,10 @@
+require("dotenv").config;
+
+const MONGO_DB_CONFIG={
+    DB:'mongodb+srv://ronsard44djoumessi:lpmoehE4NPQDts8Q@backend-repo11.5w67zxp.mongodb.net/Backend-repo11Project?retryWrites=true&w=majority'
+}
+
+
+module.exports = {
+    MONGO_DB_CONFIG
+}
